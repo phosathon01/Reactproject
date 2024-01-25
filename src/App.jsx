62569cddfc6bb99bddd5ce324ad20f86e44928d6
phosathon01 <phosathon.tw@gmail.com>
2024-01-25 19:20:09 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useInsertionEffect, useState } from "react"
+import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+
+const App = () => {
+  let heroData = [
+    {text1:"Divie into",text2:"what you love"},
+    {text1:"Indulge",text2:"you passions"},
+    {text1:"Give in to",text2:"you passions"},
+  ]
+
+  const [heroCount,setHeroCount] = useState(0);
+  const [playStatus,setPlayStatus] = useState(false);
+
+useEffect(()=>{
+  setInterval(() => {
+    setHeroCount((count)=>{return count===2?0:count+1})
+  }, 3000);
+},[])
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar></Navbar>
+      <Hero 
+      setPlayStatus= {setPlayStatus} 
+      heroData={heroData[heroCount]} 
+      heroCount={heroCount}
+      setHeroCount={setHeroCount} 
+      playStatus={playStatus}
+      ></Hero>
+     
+    </div>
+  )
+}
+
+export default App
